65aa5e0ac7de860938e37b401c167d1445e38947
zakibn18 <jekben815@gmail.com>
2025-03-17 21:50:39 +0700

    deklarasi variable

--- switchCase.cpp
+++ switchCase.cpp
@@ -8,3 +8,9 @@ float LuasPersegiPanjang(float a, float b){
 float LuasSegitiga(float a, float b){
   return 0.5 * a * b;
 }
+
+int main(){
+  int pilihan;
+  float panjang, lebar;
+  float alas, tinggi;
+}
