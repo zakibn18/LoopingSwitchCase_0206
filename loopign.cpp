7a038ef734232715f37dd6788e0771ed115a00e2
zakibn18 <jekben815@gmail.com>
2025-03-17 21:06:32 +0700

    example for ascending

--- loopign.cpp
+++ loopign.cpp
@@ -1,12 +1,21 @@
 #include <iostream>
 #include <cstdlib>
 #include <ctime>
+
 using namespace std;
 
 
 int main(){
-  int couter;
+  int counter;
   int i;
   int perulanganWhile = 0;
   int perulanganDO = 0;
+
+  //Loop with for
+  //Loop for ascending
+  for(counter = 0; counter < 5; counter++) {
+    cout << "Nilai couter = " << counter << " TI UMY" << endl;
+  }
+  cout << "Nilai couter terakhir = " << counter << endl << endl;
+
 }
