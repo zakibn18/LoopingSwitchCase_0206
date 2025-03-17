f5edffd725d059c761936a9dcf47934589fcb6a7
zakibn18 <jekben815@gmail.com>
2025-03-17 21:52:32 +0700

    membuat menu pilihan

--- switchCase.cpp
+++ switchCase.cpp
@@ -13,4 +13,13 @@ int main(){
   int pilihan;
   float panjang, lebar;
   float alas, tinggi;
+
+
+    cout << "Menu\n";
+    cout << "1. Luas Persegi Panjang" << endl;
+    cout << "2. Luas Persegi Panjang" << endl;
+    cout << "3. Keluar" << endl;
+
+    cout << "Pilihan (1/2/3) = ";
+    cin >> pilihan;
 }
