453b9ddd536c63f9b88b7dd1b8675ce23fc1c30e
zakibn18 <jekben815@gmail.com>
2025-03-17 21:09:37 +0700

    examlpe for descending

--- loopign.cpp
+++ loopign.cpp
@@ -14,8 +14,14 @@ int main(){
   //Loop with for
   //Loop for ascending
   for(counter = 0; counter < 5; counter++) {
-    cout << "Nilai couter = " << counter << " TI UMY" << endl;
+    cout << "Nilai counter = " << counter << " TI UMY" << endl;
   }
-  cout << "Nilai couter terakhir = " << counter << endl << endl;
+  cout << "Nilai counter terakhir = " << counter << endl << endl;
+
+  //Loop for descending
+  for(counter = 5; counter > 0; counter--) {
+    cout << "Nilai counter = " << counter << " TI UMY" << endl;
+  }
+  cout << "Nilai counter terakhir = " << counter << endl << endl;
 
 }
